4353755714197485d3c3dd53a83e0b05f1bf0a47
Navar M M N <navarmedeiros@gmail.com>
2018-07-27 18:02:04 -0300

    beta version

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,4 +39,98 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  ////////////////////////////////////////////////////////
+
+  // Change color of button in education setion while pressed
+
+  // var phd_button = $('#phd_description')
+  var toogle = false;
+  var lastButton;
+
+  $(document).ready(function () {
+
+    if ($('#phd_description')[0].classList['value'] == "collapse"  &&
+        $('#msc1_description')[0].classList['value'] == "collapse" &&
+        $('#msc2_description')[0].classList['value'] == "collapse" &&
+        $('#bsc_description')[0].classList['value'] == "collapse") {
+
+          $(('#phd_button')).css('color', 'black');
+          $(('#phd_button')).css('background-color', 'white');
+
+          $(('#msc1_button')).css('color', 'black');
+          $(('#msc1_button')).css('background-color', 'white');
+
+          $(('#msc2_button')).css('color', 'black');
+          $(('#msc2_button')).css('background-color', 'white');
+
+          $(('#bsc_button')).css('color', 'black');
+          $(('#bsc_button')).css('background-color', 'white');
+        }
+
+
+    $('.btn-light').click(function(){
+      if (lastButton == null) {
+        if (toogle == false) {
+          $(this).css('color', 'white');
+          $(this).css('background-color', 'black');
+          toogle = true;
+          lastButton = $(this)[0].id;
+        }
+        else {
+          $(this).css('color', 'black');
+          $(this).css('background-color', 'white');
+        }
+      }
+      // List not NULL
+      else {
+        if (lastButton == $(this)[0].id) {
+
+        }
+        else {
+          if (toogle == true) {
+            // lastButton:
+            $(('#' + lastButton)).css('color', 'black');
+            $(('#' + lastButton)).css('background-color', 'white');
+            $(this).css('color', 'white');
+            $(this).css('background-color', 'black');
+            toogle = true;
+            lastButton = $(this)[0].id;
+          }
+          else {
+            // lastButton:
+            $(('#' + lastButton)).css('color', 'white');
+            $(('#' + lastButton)).css('background-color', 'black');
+            $(this).css('color', 'black');
+            $(this).css('background-color', 'white');
+            toogle = true;
+            lastButton = $(this)[0].id;
+          }
+        }
+      }
+
+    })
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })(jQuery); // End of use strict
